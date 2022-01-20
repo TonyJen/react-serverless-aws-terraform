1963d32da01a207bbc4437908979c988467ca016
Tony Jen <tonyjen0905@gmail.com>
2022-01-19 19:21:08 -0500

    Add AWSLambdaSQSQueueExecutionRole  permission

--- deploy/10-lambdas-todos.tf
+++ deploy/10-lambdas-todos.tf
@@ -130,6 +130,11 @@ resource "aws_iam_role_policy_attachment" "basic" {
   role       = aws_iam_role.lambda_exec.id
 }
 
+resource "aws_iam_role_policy_attachment" "basic" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaSQSQueueExecutionRole "
+  role       = aws_iam_role.lambda_exec.id
+}
+
 resource "aws_iam_role_policy" "lambda_policy" {
   name = "lambda_policy"
   role = aws_iam_role.lambda_exec.id
