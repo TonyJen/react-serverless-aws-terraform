9d4b3d55cd83151c0c547bf23829f716340315be
Tony Jen <tonyjen0905@gmail.com>
2022-06-06 21:21:01 -0400

    Update layout and code.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -108,7 +108,7 @@ const HomePage = () => {
               <Card
                 key={todo.todoId ? todo.todoId.S : index}
                 title={todo.name.S ? todo.name.S : todo.name}
-                style={{ width: 300 }}
+                style={{ width: 300, padding: "0 50px" }}
               >
                 <p>
                   {todo.description.S ? todo.description.S : todo.description}
