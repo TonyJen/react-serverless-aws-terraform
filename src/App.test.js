eb0edd41e4859e07c8cacd5006162907d0d37777
Tony Jen <tonyjen0905@gmail.com>
2022-10-25 19:14:41 -0400

    Revert "Revert "reset branch.""
    This reverts commit 2b747262cea47f8e8af3d95513ec6539789f9605.

--- src/App.test.js
+++ src/App.test.js
@@ -4,7 +4,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const x = "To-do list powered by AWS serverless architecture";
-  const linkElement = getByText(new RegExp(x, "i"));
-  //expect(linkElement).toBeInTheDocument();
+  const x = 'To-do list powered by AWS serverless architecture';
+  const linkElement = getByText(new RegExp(x, 'i'));
+  // expect(linkElement).toBeInTheDocument();
 });
