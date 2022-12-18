b675c8b2d8f7b8687520d4bc85149ef158170d1b
Tony Jen <tonyjen0905@gmail.com>
2022-12-17 19:59:42 -0500

    update lookup

--- deploy/lambdas/getTodos.js
+++ deploy/lambdas/getTodos.js
@@ -9,31 +9,48 @@ const ddb = new AWS.DynamoDB({ apiVersion: "2012-08-10" });
 
 exports.handler = function(event, context, callback) {
   console.log('event', event);
+  const cognitoIdentityId = event.requestContext.identity.cognitoIdentityId;
+  console.log(cognitoIdentityId)
   const params = {
-    TableName: "todos"
+    TableName: "todos",
+    FilterExpression: `identityId = :val`,
+    ExpressionAttributeValues: {
+      ':val': { S: cognitoIdentityId }
+    }
   };
 
   let responseCode = 200;
   let responseBody = "";
 
-  ddb.scan(params, function(err, data) {
-    if (err) {
-      console.log("Error", err);
-      responseCode = 500;
-      responseBody = err;
-    } else {
-      console.log("Success", data);
-      responseBody = data;
-    }
-    const response = {
-      statusCode: responseCode,
-      headers: {
-        "content-type": "application/json",
-        "Access-Control-Allow-Origin": "*"
-      },
-      body: JSON.stringify(responseBody)
-    };
+  const scanTable = function() {
+    ddb.scan(params, function(err, data) {
+      if (err) {
+        console.log("Error", err);
+        responseCode = 500;
+        responseBody = err;
+      } else {
+        console.log("Success", data);
+        responseBody = data;
+        if (typeof data.LastEvaluatedKey != 'undefined') {
+          params.ExclusiveStartKey = data.LastEvaluatedKey;
+          scanTable();
+        }
+        const response = {
+          statusCode: responseCode,
+          headers: {
+            "content-type": "application/json",
+            "Access-Control-Allow-Origin": "*"
+          },
+          body: JSON.stringify(responseBody)
+        };
+        
+        callback(null, response);
+      }
+    })
+  }
+
+  scanTable()
   
-    callback(null, response);
-  });
 };
+
+
